7630d5f7da91ad54ccda260596fba347c1647287
Washi <lorisweier@gmx.fr>
2022-10-13 14:24:39 +0200

    ajout réseaux

---
+++ main.js
@@ -0,0 +1,9 @@
+function show_img() {
+
+    var x = document.getElementById("discord-profil");
+    if (x.style.display == 'inline') {
+        x.style.display='none';
+    } else {
+        x.style.display='inline';
+    }
+}
